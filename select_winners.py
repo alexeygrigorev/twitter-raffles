92f70558f12095337d0bbfd49eb5fcbfa1a98561
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2022-08-03 07:58:15 +0200

    bug fixes and removing data

--- select_winners.py
+++ select_winners.py
@@ -29,26 +29,35 @@ def main(remove=True):
     url = sys.argv[1]
     handle, tweet_id = util.parse_url(url)
 
-    token = util.get_twitter_auth_token()
-    folowers = util.get_twitter_followers(handle, token)
-
     retweets_folder = Path(f'retweets/{tweet_id}/')
-    
+    if not retweets_folder.exists():
+        print(f"{retweets_folder} doesn't exist, exiting...")
+        return
+
     files = sorted(retweets_folder.glob('*.json'))
     retweeters = read_all_retweeters(files)
     print(retweeters)
 
+    token = util.get_twitter_auth_token()
+    folowers = util.get_twitter_followers(handle, token)
+
     valid_participants = list(retweeters.keys() & folowers)
     shuffle(valid_participants)
 
-    winners = valid_participants[:config.top_winners]
+    top = config.top_winners
+    winner_ids = valid_participants[:top]
+    winners = [retweeters[i] for i in winner_ids]
 
     winners_file = Path(f'winners/{tweet_id}.txt')
     winners_file.parent.mkdir(parents=True, exist_ok=True)
 
+    print('winners:')
+    print()
+
     with winners_file.open('wt', encoding='utf8') as f_out:
         for winner in winners:
             f_out.write(f'@{winner}\n')
+            print(f'@{winner}')
 
     if remove == True:
         for file in files:
